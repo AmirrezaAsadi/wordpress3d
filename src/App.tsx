d6c93d7de98f9729cecc6be5dc7357f23d95365d
Amir Reza Asadi <asadiaa@mail.uc.edu>
2025-02-19 22:23:29 -0500

    Remove NetworkVisualization component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import NetworkVisualization from './components/NetworkVisualization'
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <NetworkVisualization />
     </div>
   )
 }
